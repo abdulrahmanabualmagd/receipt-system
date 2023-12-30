fef991ef4fb3dca6453619c1ebb5501c4550a066
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-12-30 15:30:25 +0200

    Add IComparable

---
+++ Collections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Collections
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ IComparable&IComparer/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IComparable
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person p1 = new Person(1, "ahmed");
+            Person p2 = new Person(1, "ahmed");
+
+            Console.WriteLine(p1.CompareTo(p2));
+            //  1 => p1 > p2
+            //  0 => p1 = p2
+            // -1 => p1 < p2
+        }
+    }
+}
