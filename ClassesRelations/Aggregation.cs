8ac2888ef8f3b7fcf73f1c657548ad6fa36fc05f
Abdularhman Ahmed <abdwbedoo@gmail.com>
2023-12-30 20:12:17 +0200

    Add Classes Relations

---
+++ ClassesRelations/Aggregation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregation
+{
+    public class A
+    {
+
+    }
+
+    // Class B contain Class A as a member but not depent on it unlike Composition
+    public class B
+    {
+        A a;    
+    }
+}
---
+++ ClassesRelations/Inheritance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class A
+    {
+
+    }
+
+    // (child)Inherits Properties from Class A(parent)
+    public class B : A
+    {
+
+    }
+}
