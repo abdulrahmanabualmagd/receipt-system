6142eb227f74fd211c340995da36e8be1584612a
Abdularhman Ahmed <abdwbedoo@gmail.com>
2024-01-01 08:57:42 +0200

    Add Extension Method

---
+++ Extension_Methods/ExtensionMethod.cs
@@ -0,0 +1,18 @@
+﻿using Constructor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods
+{
+    internal static class ExtensionMethod
+    {
+        // An Extension Method for Person Class
+        internal static void Show(this Person data)
+        {
+            Console.WriteLine($"{data.Id} - {data.Name}");
+        }
+    }
+}
---
+++ Extension_Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * - A feature in C# that allows you to add new methods to existing types without modifying them
+ * - Are defined in static classes and are marked with 'this' keyword before the first parameter, 
+ *   indicating the type that the method extends
+ */
+
+using Constructor;
+
+namespace Extension_Methods
+{
+    class Protram
+    {
+        static void Main()
+        {
+            // Create Instance from Person
+            Person person = new Person(0, "Abdulrahman");
+
+            // Use Extension Method
+            person.Show();
+        }
+    }
+}
