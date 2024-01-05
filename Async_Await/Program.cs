77e4fd4fc90444303cac09f3a8a8a2d76cfb4b0a
Abdularhman Ahmed <abdwbedoo@gmail.com>
2024-01-05 15:18:36 +0200

    Add Async_Await

---
+++ Async_Await/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Async_Await
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Start Main Method");
+            
+            // The control will not be blocked with the 'DoAsyncWork' method it will return to main until the Task is completed
+            // await keyword must be inside an async method
+            // We use await with the tasks that could take too long time to load so we use it to avoid blocking the control and be responsive with other acitons
+            await DoAsyncWork();
+
+            Console.WriteLine("End Main Method");
+        }
+
+        static async Task DoAsyncWork()
+        {
+            Console.WriteLine("Start Asynch Work...");
+
+
+            // Because of using await, the contorl will not be blocked, instead it will return to the main method 
+            await Task.Delay(2000);
+
+            Console.WriteLine("End of Async Work...");
+        }
+    }
+}
