f632de67b2ecddf8def08e476d3ebb17abd4c2f5
Abdularhman Ahmed <abdwbedoo@gmail.com>
2024-01-07 15:31:16 +0200

    Add Generic Repository

---
+++ MVC Core/IRepositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * - Generic Repository Interface:
+ *      - This interface defines a standard set of methods for generic CRUD operations, promoting 
+ *      - abstraction and separation of concerns in data access. It serves as a modular foundation, 
+ *      - allowing easy creation of concrete repositories for various entity types. The repository 
+ *      - pattern enhances code maintainability, flexibility, and testability by encapsulating data 
+ *      - access logic and providing a consistent interface across the application.
+ *      
+ *      We are using bool return as a feedback to know if the operation is completed successfully or not
+ */
+namespace MVC_Core.IRepositories
+{
+    public interface IRepository<T>
+    {
+        Task<T> GetById(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task<bool> Add(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+        Task<int> Count();
+    }
+}
