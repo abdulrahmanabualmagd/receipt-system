b0bdbd981bd60c65ec6819b29aafb0ee7c9d2dae
Abdularhman Ahmed <abdwbedoo@gmail.com>
2024-01-07 15:28:17 +0200

    Add Home Controller

--- MVC Core/Controllers/HomeController.cs
+++ MVC Core/Controllers/HomeController.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Core.IRepositories;
+using MVC_Core.Models;
 
 namespace MVC_Core.Controllers
 {
     public class HomeController : Controller
     {
+        #region Dependency Injection
+        private readonly IRepository<Student> _studentRepository;
+        private readonly IRepository<School> _schoolRepository;
+
+        public HomeController(IRepository<Student> studentRepository, IRepository<School> schoolRepository)
+        {
+            _studentRepository = studentRepository;
+            _schoolRepository = schoolRepository;
+        } 
+        #endregion
+
         public IActionResult Home()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Details(int id)
         {
             return View();
         }
