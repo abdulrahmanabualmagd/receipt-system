be59c23e304f0a97f6fc50bfba99ccf2339042a6
Abdularhman Ahmed <abdwbedoo@gmail.com>
2024-02-25 15:05:11 +0200

    Remove Unnecessary Entities

---
+++ Core/DTOs/ProfileReceiptDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class ProfileReceiptDto
+    {
+        public decimal TotalAmount { get; set; }
+        public decimal PaidAmount { get; set; }
+        public DateTime ReleasedDate { get; set; }
+
+    }
+}
--- Core/Entities/BaseEntity/Entity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Core.Entities.BaseEntity
-{
-    public class Entity       // Used as a constraint for generic types. Means that any class must contain these properties in this class
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-    }
-}
